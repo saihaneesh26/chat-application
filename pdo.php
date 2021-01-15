7e6078dea11a90d6e8869b78513ef5adce9d6ff6
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-01-15 15:21:52 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,3 +1,8 @@
 <?php
-$pdo=new PDO('mysql:host=localhost;port=3306;dbname=project','root','');
+$host=ec2-52-205-61-60.compute-1.amazonaws.com;
+$username=oygvnaphenntha;
+ $pass=45bd7bcf0a71f96b94a827d1e61e0ef9b52ab0fb77366f6cb7f621eb79cc7e1c;
+$port=5432;
+$dbname=dc85gdddpisuug;
+$pdo=new PDO('pgsql:host='.$host.';port='.$port.';dbname='.$dbname,$username,$pass);
 ?>
