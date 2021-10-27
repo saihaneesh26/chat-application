3fa39fba53c2059f5a86a16c425b62ba05161f92
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-10-27 16:21:24 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,3 +1,3 @@
 <?php
-$pdo=new PDO('pgsql:host=ec2-52-205-61-60.compute-1.amazonaws.com;port=5432;dbname=dc85gdddpisuug','oygvnaphenntha','45bd7bcf0a71f96b94a827d1e61e0ef9b52ab0fb77366f6cb7f621eb79cc7e1c');
+$pdo=new PDO('');
 ?>
